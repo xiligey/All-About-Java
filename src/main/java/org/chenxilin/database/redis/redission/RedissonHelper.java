3abd4b3207f973dbead7f9fbbf854ab605c45d7b
chenxilin <635943647@qq.com>
2023-03-13 18:42:36 +0800

    feat: redisson Helper

--- src/main/java/org/chenxilin/database/redis/redission/RedissonHelper.java
+++ src/main/java/org/chenxilin/database/redis/redission/RedissonHelper.java
@@ -2,7 +2,6 @@ package org.chenxilin.database.redis.redission;
 
 import org.redisson.Redisson;
 import org.redisson.api.RBucket;
-import org.redisson.api.RMap;
 import org.redisson.api.RedissonClient;
 import org.redisson.client.codec.StringCodec;
 import org.redisson.config.Config;
@@ -10,7 +9,7 @@ import org.redisson.config.Config;
 /**
  * @author chenxilin
  */
-public class RedissionHelper {
+public class RedissonHelper {
     public static void main(String[] args) {
         Config config = new Config();
         config.useSingleServer().setAddress("redis://192.168.100.194:6379").setPassword("aiops-test");
